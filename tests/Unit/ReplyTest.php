42a49abb2ef281cef506b4c791edd256f3421ea0
Shobi P P <ppshobi@gmail.com>
2017-09-16 00:46:00 +0530

    Test for Reply

---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ReplyTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    function it_has_an_owner()
+    {
+        $reply = factory('App\Reply')->create();
+
+        $this->assertInstanceOf('App\User', $reply->owner);
+    }
+}
