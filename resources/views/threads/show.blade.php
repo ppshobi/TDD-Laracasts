c06f1e0309889022797dd8f0000ba77aa28fc1c0
Shobi P P <ppshobi@gmail.com>
2017-09-16 00:52:32 +0530

    small style

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,7 +19,9 @@
             @foreach($thread->replies as $reply)
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                       {{ $reply->owner->name }} said {{ $reply->created_at->diffForHumans() }}
+                        <a href="#"> 
+                        {{ $reply->owner->name }}
+                        </a> said {{ $reply->created_at->diffForHumans() }} ...
                     </div>
                     <div class="panel-body">
                         {{ $reply->body }}
