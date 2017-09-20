a291d254cf8cc8f88019067841f897a694e340e4
Shobi P P <ppshobi@gmail.com>
2017-09-20 22:31:42 +0530

    create threads view

---
+++ resources/views/threads/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Create a New Thread</div>
+
+                <div class="panel-body">
+                    <form action="">
+                        <div class="form-group">
+                            <label for="title"> Title </label>
+                            <input type="text" class="form-control" id="title" placeholder="title">
+                        </div> 
+
+                        <div class="form-group">
+                            <label for="title"> Body </label>
+                            <textarea class="form-control" id="body"> </textarea>
+                        </div> 
+                        <button class="btn btn-default" type="submit">Publish</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
