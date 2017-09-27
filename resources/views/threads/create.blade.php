00487e371a864d914975cbe16d435c0c894bc6d0
Shobi P P <ppshobi@gmail.com>
2017-09-27 23:00:04 +0530

    remember selected channel in case of a validation error, UI Improvements

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -13,8 +13,11 @@
                             <div class="form-group">
                                 <label for="channel_id">Choose a Channel</label>
                                 <select name="channel_id" id="channel_id" class="form-control">
+                                    <option value="">Choose One...</option>
                                     @foreach(App\Channel::all() as $channel)
-                                        <option value="{{ $channel->id }}">{{ $channel->name }}</option>
+                                        <option value="{{ $channel->id }}" {{old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                            {{ $channel->name }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
