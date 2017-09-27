8047f7cedeb088b9e35e4eb6e47293bbe69a0c5f
Shobi P P <ppshobi@gmail.com>
2017-09-27 21:44:18 +0530

    refactors

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -17,13 +17,13 @@ class ThreadsController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param Channel $channel
      * @return \Illuminate\Http\Response
      */
-    public function index($channelSlug = null)
+    public function index(Channel $channel)
     {
-        if ($channelSlug){
-            $channelId = Channel::where('slug',$channelSlug)->first()->id;
-            $threads = Thread::where('channel_id', $channelId)->get();
+        if ($channel->exists){
+            $threads = $channel->threads()->latest()->get();
         }else{
             $threads = Thread::latest()->get();
         }
