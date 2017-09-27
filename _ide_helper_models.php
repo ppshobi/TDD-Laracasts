2bc7e5961c61b54ea91244e4cdad3947a2096c62
Shobi P P <ppshobi@gmail.com>
2017-09-27 21:52:33 +0530

    installed barryvdh/laravel-ide-helper, so php storm can hint code better

---
+++ _ide_helper_models.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\Reply
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $thread_id
+ * @property string $body
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\User $owner
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Reply whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Reply whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Reply whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Reply whereThreadId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Reply whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Reply whereUserId($value)
+ */
+	class Reply extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Thread
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $channel_id
+ * @property string $title
+ * @property string $body
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\Channel $channel
+ * @property-read \App\User $owner
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Reply[] $replies
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereChannelId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereUserId($value)
+ */
+	class Thread extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Channel
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Thread[] $threads
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Channel whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Channel whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Channel whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Channel whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Channel whereUpdatedAt($value)
+ */
+	class Channel extends \Eloquent {}
+}
+
