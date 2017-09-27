6f48882b785149314b6aba04f7551488f9897a17
Shobi P P <ppshobi@gmail.com>
2017-09-28 00:25:18 +0530

    User can filter threads by username

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Channel;
 use App\Thread;
+use App\User;
 use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
@@ -22,11 +23,19 @@ class ThreadsController extends Controller
      */
     public function index(Channel $channel)
     {
-        if ($channel->exists){
-            $threads = $channel->threads()->latest()->get();
-        }else{
-            $threads = Thread::latest()->get();
+        if ($channel->exists) {
+            $threads = $channel->threads()->latest();
+        } else {
+            $threads = Thread::latest();
         }
+
+        if ($username = request('by')) {
+            $user = User::where('name', $username)->firstOrFail();
+            $threads->where('user_id', $user->id);
+        }
+
+        $threads = $threads->get();
+
         return view('threads.index', compact('threads'));
     }
 
--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -5,68 +5,81 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ReadThreadsTest extends TestCase
-{
+class ReadThreadsTest extends TestCase {
+
     use RefreshDatabase;
-    
+
     private $thread;
-    
-    
+
+
     public function setUp()
     {
         parent::setUp();
-        
         $this->thread = create('App\Thread');
     }
-    
+
     /**
      * A Test
-     * 
+     *
      * @test
      * @return void
      */
     public function a_user_can_browse_threads()
-    {   
+    {
         $response = $this->get('/threads');
         $response->assertSee($this->thread->title);
     }
 
     /**
      * A Test
-     * 
+     *
      * @test
      * @return void
      */
     public function a_user_can_browse_a_single_thread()
-    {   
+    {
         $response = $this->get($this->thread->path());
         $response->assertSee($this->thread->title);
     }
 
     /**
      * A Test
-     * 
+     *
      * @test
      * @return void
      */
     public function user_can_see_replies_associated_with_a_thread()
-    {   
+    {
         $reply = create('App\Reply', ['thread_id' => $this->thread->id]);
-
         $response = $this->get($this->thread->path());
         $response->assertSee($reply->body);
     }
 
     /**
-    * @test
-    *
-    */
-    public function a_user_can_filter_threads_based_on_channel_tags(){
+     * @test
+     *
+     */
+    public function a_user_can_filter_threads_based_on_channel_tags()
+    {
         $channel = create('App\Channel');
-        $threadInChannel = create('App\Thread', ['channel_id'=> $channel->id]);
+        $threadInChannel = create('App\Thread', ['channel_id' => $channel->id]);
         $threadNotInChannel = create('App\Thread');
         $this->withoutExceptionHandling()->get('threads/' . $channel->slug)
             ->assertSee($threadInChannel->title)
             ->assertDontSee($threadNotInChannel->title);
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function a_user_can_filter_threads_by_username()
+    {
+        $this->signIn(create('App\User', ['name' => 'Shobi']));
+        $threadByShobi = create('App\Thread', ['user_id' => auth()->id()]);
+        $threadByOtherGuy = create('App\Thread');
+        $this->get('threads?by=Shobi')
+            ->assertSee($threadByShobi->title)
+            ->assertDontSee($threadByOtherGuy->title);
     }
 }
