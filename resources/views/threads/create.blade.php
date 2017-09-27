8eb18bdfb887cb0a2207082d2658d6874192978f
Shobi P P <ppshobi@gmail.com>
2017-09-27 22:53:56 +0530

    create threads, form validation and option to select channel for a thread

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -1,29 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Create a New Thread</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Create a New Thread</div>
 
-                <div class="panel-body">
-                    <form action="/threads" method="post">
-                        {{ csrf_field() }}
-                        <div class="form-group">
-                            <label for="title"> Title </label>
-                            <input type="text" class="form-control" id="title" name="title" placeholder="Thread Title">
-                        </div> 
+                    <div class="panel-body">
+                        <form action="/threads" method="post">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="channel_id">Choose a Channel</label>
+                                <select name="channel_id" id="channel_id" class="form-control">
+                                    @foreach(App\Channel::all() as $channel)
+                                        <option value="{{ $channel->id }}">{{ $channel->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="title"> Title </label>
+                                <input type="text" class="form-control" id="title" name="title"
+                                       value="{{ old('title') }}" placeholder="Thread Title">
+                            </div>
 
-                        <div class="form-group">
-                            <label for="title"> Body </label>
-                            <textarea name="body" class="form-control" rows="8" id="body"> </textarea>
-                        </div> 
-                        <button class="btn btn-primary" type="submit">Publish</button>
-                    </form>
+                            <div class="form-group">
+                                <label for="title"> Body </label>
+                                <textarea name="body" class="form-control" rows="8"
+                                          id="body">{{ old('body') }} </textarea>
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary" type="submit">Publish</button>
+                            </div>
+                            @if(count($errors))
+                                <ul class="alert alert-danger">
+                                    @foreach($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            @endif
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
