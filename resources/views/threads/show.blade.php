3676f4edacbdd2063bf2a1f23984a8b896e55a9e
Shobi P P <ppshobi@gmail.com>
2017-09-29 17:44:59 +0530

    design with a sidebar, and show replies count in there

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <a href="#">{{ $thread->owner->name }} Posted:</a>                
@@ -14,33 +14,35 @@
                    {{ $thread->body }}
                 </div>
             </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
             @foreach($thread->replies as $reply)
                 @include('threads.reply')
             @endforeach
-        </div>
-    </div>
-
-    @if(auth()->check())
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            @if(auth()->check())
                 <form action="{{ $thread->path() . '/replies' }}" method="POST">
                     {{ csrf_field() }}
                     <div class="form-group">
-                        <textarea name="body" id="body" rows="5" class="form-control" placeholder="Say Something">
-                        
-                        </textarea>
+                <textarea name="body" id="body" rows="5" class="form-control" placeholder="Say Something">
+
+                </textarea>
                     </div>
                     <button class="btn btn-default" type="submit"> Post </button>
                 </form>
+            @else
+                <p class="text-center"><a href="{{ route('login') }}">Sign in</a> to post Comments </p
+            @endif
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <p>This Thread was published {{ $thread->created_at->diffForHumans() }} by
+                        <a href="#"> {{ $thread->owner->name }} </a> and currently has {{ $thread->replies_count }}
+                        {{ str_plural('comment', $thread->replies_count) }}
+                    </p>
+                </div>
             </div>
         </div>
-    @else
-        <p class="text-center"> <a href="{{ route('login') }}">Sign in</a> to post Comments  </p
-    @endif
+    </div>
+
+
 </div>
 @endsection
