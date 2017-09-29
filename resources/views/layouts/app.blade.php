395ab162a76875b9e141717c3fdda6cf64e8aa93
Shobi P P <ppshobi@gmail.com>
2017-09-29 19:30:55 +0530

    popular threads link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,7 @@
                                aria-haspopup="true" aria-expanded="false">Browse<span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li><a href="/threads">All Threads</a></li>
+                                <li><a href="/threads?popular=1">Popular Threads</a></li>
                                 @if(auth()->check())
                                     <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a></li>
                                 @endif
