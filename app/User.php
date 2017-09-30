262bd70d41bece473692f74891ac438caf31fd84
Shobi P P <ppshobi@gmail.com>
2017-10-01 00:38:50 +0530

    override route model binding key name

--- app/User.php
+++ app/User.php
@@ -9,6 +9,10 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function getRouteKeyName ()
+    {
+        return 'name';
+    }
     /**
      * The attributes that are mass assignable.
      *
