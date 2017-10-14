e399d1ea7ee110eea5584def2b54fd1c74404e9d
Shobi P P <ppshobi@gmail.com>
2017-10-14 23:28:25 +0530

    dedicated activity component, and code refactorings for blade templates

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -6,7 +6,6 @@
             <div class="page-header">
                 <h1>
                     {{ $profileUser->name }}
-                    <small>Since {{ $profileUser->created_at->diffForHumans() }}...</small>
                 </h1>
             </div>
             <div>
