df102d2b1096ddbf531127367ff79b4609004c90
Shobi P P <ppshobi@gmail.com>
2017-10-14 23:49:22 +0530

    migrate controller queries to Activity model

--- app/Activity.php
+++ app/Activity.php
@@ -8,8 +8,27 @@ class Activity extends Model
 {
     protected $guarded = [];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
     public function subject()
     {
         return $this->morphTo();
+    }
+
+    /**
+     * @param User $user
+     * @return $activities
+     */
+    public static function feed(User $user)
+    {
+        return $user->activity()
+            ->latest()
+            ->with('subject')
+            ->take(50)
+            ->get()
+            ->groupBy(function ($activity) {
+                return $activity->created_at->format('Y-m-d');
+            });
     }
 }
--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use App\User;
 
 /**
@@ -18,23 +19,8 @@ class ProfilesController extends Controller
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => $this->getActivity($user),
+            'activities' => Activity::feed($user),
         ]);
     }
 
-    /**
-     * @param User $user
-     * @return $activities
-     */
-    protected function getActivity(User $user)
-    {
-        return $user->activity()
-            ->latest()
-            ->with('subject')
-            ->take(50)
-            ->get()
-            ->groupBy(function ($activity) {
-                return $activity->created_at->format('Y-m-d');
-            });
-    }
 }
