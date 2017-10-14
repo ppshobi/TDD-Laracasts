d7caec2026efd9f6dc57d405d9ed617d13ba9491
Shobi P P <ppshobi@gmail.com>
2017-10-14 23:45:22 +0530

    test for `Activity::feed()` method

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Activity;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -42,5 +43,27 @@ class ActivityTest extends TestCase
         $reply = create('App\Reply');
 
         $this->assertEquals(2, Activity::count());
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function it_fetches_activity_feed_for_any_user()
+    {
+        $this->signIn();
+
+        create('App\Thread', ['user_id' => auth()->id(),]);
+
+        create('App\Thread', [
+            'user_id' => auth()->id(),
+            'created_at' => Carbon::now()->subWeek(),
+        ]);
+
+        $feed = Activity::feed(auth()->user());
+
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->format('Y-m-d')
+        ));
     }
 }
