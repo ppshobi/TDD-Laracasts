1b4f4adeb4c2f7d85da9a505e191800fa49c0c85
Shobi P P <ppshobi@gmail.com>
2017-12-10 23:55:18 +0530

    progress in migrating blade file to view template

--- resources/assets/js/components/Reply.vue
+++ resources/assets/js/components/Reply.vue
@@ -1,15 +1,14 @@
 <template>
-    <div id="reply-{{ $reply->id }}" class="panel panel-default">
+    <div :id="'reply-'+id" class="panel panel-default">
         <div class="panel-heading">
             <div class="level">
                 <h5 class="flex">
-                    <a href="{{ route('profile', $reply->owner->name) }}">
-                        {{ $reply->owner->name }}
-                    </a> said {{ $reply->created_at->diffForHumans() }}
+                    <a href="'/profiles/' + data.owner.name" v-text="data.owner.name">
+                    </a> said {{ data.created_at }}...
                 </h5>
-                @if(Auth::check())
-                <favorite :reply="{{ $reply }}"></favorite>
-                @endif
+                <!--@if(Auth::check())-->
+                    <!--<favorite :reply="{{ $reply }}"></favorite>-->
+                <!--@endif-->
             </div>
         </div>
         <div class="panel-body">
@@ -24,12 +23,12 @@
 
             <div v-else v-text="body"></div>
         </div>
-        @can('update', $reply)
-        <div class="panel-footer level">
-            <button class="btn btn-xs mr-1" @click="editing = true">Edit</button>
-            <button class="btn btn-xs btn-danger" @click="destroy">Delete</button>
-        </div>
-        @endcan
+        <!--@can('update', $reply)-->
+            <!--<div class="panel-footer level">-->
+                <!--<button class="btn btn-xs mr-1" @click="editing = true">Edit</button>-->
+                <!--<button class="btn btn-xs btn-danger" @click="destroy">Delete</button>-->
+            <!--</div>-->
+        <!--@endcan-->
     </div>
 </template>
 <script>
@@ -42,6 +41,7 @@
         data() {
             return {
                 editing: false,
+                id: this.data.id,
                 body: this.data.body,
             };
         },
