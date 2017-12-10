24247521ba0232dc971cfd4deb9392d255507b56
Shobi P P <ppshobi@gmail.com>
2017-12-10 23:48:42 +0530

    migrate reply.blade file to Reply.vue

--- resources/assets/js/components/Reply.vue
+++ resources/assets/js/components/Reply.vue
@@ -1,3 +1,37 @@
+<template>
+    <div id="reply-{{ $reply->id }}" class="panel panel-default">
+        <div class="panel-heading">
+            <div class="level">
+                <h5 class="flex">
+                    <a href="{{ route('profile', $reply->owner->name) }}">
+                        {{ $reply->owner->name }}
+                    </a> said {{ $reply->created_at->diffForHumans() }}
+                </h5>
+                @if(Auth::check())
+                <favorite :reply="{{ $reply }}"></favorite>
+                @endif
+            </div>
+        </div>
+        <div class="panel-body">
+            <div v-if="editing">
+                <div class="form-group">
+                    <textarea class="form-control" v-model="body"></textarea>
+                </div>
+
+                <button class="btn btn-xs btn-primary" @click="update">Update</button>
+                <button class="btn btn-xs btn-link" @click="editing=false ">Cancel</button>
+            </div>
+
+            <div v-else v-text="body"></div>
+        </div>
+        @can('update', $reply)
+        <div class="panel-footer level">
+            <button class="btn btn-xs mr-1" @click="editing = true">Edit</button>
+            <button class="btn btn-xs btn-danger" @click="destroy">Delete</button>
+        </div>
+        @endcan
+    </div>
+</template>
 <script>
     import Favorite from './Favorite.vue'
     export default {
