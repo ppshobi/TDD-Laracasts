e1b57ea68430dd977e35f6c74e9d8a50f851f2f6
Shobi P P <ppshobi@gmail.com>
2017-12-20 22:37:17 +0530

    progress on vue paginator

--- resources/assets/js/components/Replies.vue
+++ resources/assets/js/components/Replies.vue
@@ -3,7 +3,7 @@
         <div v-for="(reply, index) in items" :key="reply.id">
             <reply :data="reply" @deleted="remove(index)"></reply>
         </div>
-
+        <paginator></paginator>
         <new-reply @created="add" :endpoint="endpoint"></new-reply>
     </div>
 </template>
@@ -11,14 +11,16 @@
 <script>
     import Reply from "./Reply.vue";
     import NewReply from './NewReply.vue';
+    import collection from '../mixins/Collection';
 
     export default {
 
         components: {Reply, NewReply},
+        mixins:[collection],
 
         data() {
             return {
-                items: [],
+                dataSet: false,
                 endpoint: location.pathname + '/replies',
             };
         },
@@ -38,19 +40,11 @@
                 return `${location.pathname}/replies`;
             },
 
-            refresh(response) {
-                console.log(response);
-            },
+            refresh({data}) {
+                this.dataSet = data;
+                this.items   = data.data;
 
-            remove(index) {
-                this.items.splice(index, 1);
-                this.$emit('removed');
-                flash('Reply Deleted');
-            },
-
-            add(reply){
-                this.items.push(reply);
-                this.$emit('added');
+                // console.log(data);
             },
         }
     }
