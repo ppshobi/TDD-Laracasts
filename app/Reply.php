2ccf94ccea3b493e94c59dde134c49a53b6b6b4d
Shobi P P <ppshobi@gmail.com>
2017-12-21 01:37:38 +0530

    replies count updated when a reply is deleted + test for it

--- app/Reply.php
+++ app/Reply.php
@@ -21,6 +21,11 @@ class Reply extends Model
         static::created(function($reply){
             $reply->thread->increment('replies_count');
         });
+
+        static::deleted(function($reply){
+            $reply->thread->decrement('replies_count');
+        });
+
     }
 
 
