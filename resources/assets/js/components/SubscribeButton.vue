ff1b81d2c93ac3f19337bdc9665c7db97aa49fc3
Shobi P P <ppshobi@gmail.com>
2017-12-22 02:18:06 +0530

    functional subscribe button component

--- resources/assets/js/components/SubscribeButton.vue
+++ resources/assets/js/components/SubscribeButton.vue
@@ -1,15 +1,24 @@
 <template>
-    <button class="btn btn-default" @click="subscribe">Subscribe</button>
+    <button :class="classes" @click="subscribe">Subscribe</button>
 </template>
 
 <script>
     export default {
+        props: ['active'],
+
+        computed: {
+            classes() {
+                return ['btn', this.active ? 'btn-primary' : 'btn-default'];
+            }
+        },
+
         methods: {
             subscribe(){
                 axios.post(location.pathname + '/subscriptions');
                 flash('Subscribed');
             },
-        }
+        },
+
     }
 </script>
 
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -37,7 +37,7 @@
                                 {{ str_plural('reply', $thread->replies_count) }}
                             </p>
                             <p>
-                                <subscribe-button></subscribe-button>
+                                <subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}"></subscribe-button>
                             </p>
                         </div>
                     </div>
