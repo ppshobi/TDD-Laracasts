0bdc27c15aed381ba775655301e1c85af306836d
Shobi P P <ppshobi@gmail.com>
2017-12-22 01:12:03 +0530

    feature test for subscribing to a thread

---
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SubscribeToThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     *
+     */
+    public function a_user_can_subscribe_to_threads()
+    {
+        $thread = create('App\Thread');
+
+        $this->withoutExceptionHandling()->post($thread->path() . '/subscriptions');
+
+        $this->assertCount(1, $thread->subscriptions);
+    }
+}
