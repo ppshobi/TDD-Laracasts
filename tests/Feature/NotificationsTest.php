c59c8a97638e833c702726e7a7088410c4bc8da2
Shobi P P <ppshobi@gmail.com>
2017-12-27 23:39:23 +0530

    test to clear notifications

--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -36,4 +36,30 @@ class NotificationsTest extends TestCase
 
         $this->assertCount(1, auth()->user()->fresh()->notifications);
     }
+
+    /**
+     * @test
+     *
+     */
+    public function a_user_can_clear_a_notification()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread')->subscribe();
+
+        $thread->addReply([
+            'user_id' => create('App\User')->id,
+            'body'    => 'Some Test Reply',
+        ]);
+
+        $this->assertCount(1, auth()->user()->unreadNotifications);
+
+        $notificationId = auth()->user()->unreadNotifications->first()->id;
+
+        $this->withoutExceptionHandling()
+             ->delete("/profiles/{auth()->user()->name}/notifications/{$notificationId}");
+
+        $this->assertCount(0, auth()->user()->fresh()->unreadNotifications);
+
+    }
 }
