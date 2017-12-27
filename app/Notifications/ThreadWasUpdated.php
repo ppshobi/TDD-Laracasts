2cc3306f41e9831fb30f11476322012fac7d02a4
Shobi P P <ppshobi@gmail.com>
2017-12-27 14:50:19 +0530

    new notification

---
+++ app/Notifications/ThreadWasUpdated.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ThreadWasUpdated extends Notification
+{
+    use Queueable;
+
+    private $thread;
+    private $reply;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param $thread
+     * @param $reply
+     *
+     * @return void
+     */
+    public function __construct($thread, $reply)
+    {
+        $this->thread = $thread;
+        $this->reply = $reply;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'message' => 'TEmp Message',
+        ];
+    }
+}
