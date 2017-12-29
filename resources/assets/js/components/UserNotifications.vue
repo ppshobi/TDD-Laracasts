1ff0690da73813c3dc9833996ea653b3e2ba6d3d
Shobi P P <ppshobi@gmail.com>
2017-12-30 00:49:16 +0530

    notification template

--- resources/assets/js/components/UserNotifications.vue
+++ resources/assets/js/components/UserNotifications.vue
@@ -1,11 +1,22 @@
 <template>
-    <div>
-        Hello world
-    </div>
+    <li class="dropdown" v-if="notifications.length ">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            Notifications
+        </a>
+        <ul class="dropdown-menu">
+            <li v-for="notification in notifications">
+                <a href="#"> foobar </a>
+            </li>
+        </ul>
+    </li>
 </template>
 
 <script>
     export default {
-
+        data(){
+            return {
+                notifications: false,
+            };
+        }
     }
 </script>
