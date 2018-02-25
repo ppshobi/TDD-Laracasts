f2264416bcc9d4de064f0ee03a96869b08dba26c
Shobi P P <ppshobi@gmail.com>
2018-02-25 14:46:02 +0530

    styling

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,33 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            @forelse($threads as $thread)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="level">
-                            <h4 class="flex">
-                                <a href="{{ $thread->path() }}">
-                                    @if(auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                                        <strong>{{ $thread->title }}</strong>
-                                    @else
-                                        {{ $thread->title }}
-                                    @endif
-                                </a>
-                            </h4>
-                            <strong>
-                                <a href="{{ $thread->path() }}">
-                                    {{ $thread->replies_count }} {{ str_plural('Reply', $thread->replies_count) }}
-                                </a>
-                            </strong>
-                        </div>
-                    </div>
-                    <div class="panel-body">
-                        <div class="body"> {{ $thread->body }} </div>
-                    </div>
-                </div>
-            @empty
-                <p>There is no threads in this channel</p>
-            @endforelse
+            @include('threads._list');
         </div>
     </div>
 </div>
