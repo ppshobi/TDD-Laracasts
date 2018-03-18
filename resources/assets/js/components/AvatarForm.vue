9b9a273e365ff6cb3611277c7e25248f4184745f
Shobi P P <ppshobi@gmail.com>
2018-03-18 23:43:19 +0530

    basic avatar vue component

---
+++ resources/assets/js/components/AvatarForm.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <h1 v-text="user.name"></h1>
+        <form v-if="canUpdate" method="post" enctype="multipart/form-data">
+            <input type="file" name="avatar" accept="image/*"/>
+            <button type="submit" class="btn btn-primary"> Add Avatar</button>
+        </form>
+    <img :src="avatar" width="200" height="200"/>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['user'],
+        data(){
+            return {
+                avatar: '',
+            }
+        },
+        computed: {
+            canUpdate(){
+                return this.authorize(user => user.id === this.user.id);
+            }
+        }
+    }
+</script>
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -4,15 +4,7 @@
     <div class="container">
         <div class="col-md-8 col-md-offset-2">
             <div class="page-header">
-                <h1> {{ $profileUser->name }} </h1>
-                @can('update', $profileUser)
-                    <form method="post" action="{{ route('avatar',$profileUser) }}" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <input type="file" name="avatar"/>
-                        <button type="submit" class="btn btn-primary"> Add Avatar</button>
-                    </form>
-                @endcan
-                <img src="{{ $profileUser->avatar() }}" width="200" height="200"/>
+                <avatar-form :user="{{$profileUser}}"></avatar-form>
             </div>
             <div>
                 @forelse($activities as $date => $activity)
